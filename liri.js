98d414eb91608b8ae95a0393ceb088b3c5d40f7d
Corey <cqyates@gmail.com>
2019-04-28 20:40:22 -0400

    added inquirer

--- liri.js
+++ liri.js
@@ -12,8 +12,30 @@ var moment = require('moment');
 var omdb = require('omdb');
 var fs = require('fs');
 var axios = require("axios");
+var prompt = require("inquirer");
 
 
 
 //api keys in their own file that is not shared on github
 var spotify = new Spotify(keys.spotify);
+
+//switch function for each type of search; spotify, OMDB or BandsinTown
+function liriRun(command, searchTerm){
+    switch (command){
+        case "concert-this":
+        getBandsInTown(searchTerm);
+        break;
+
+        case "spotify-this-song":
+        getSpotify(searchTerm);
+        break;
+
+        case "movie-this":
+        getOMDB(searchTerm);
+        break;
+
+        case "do-what-it-says":
+        getRandom();
+        break;
+    }
+};
