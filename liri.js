c9e2483f57d29d5d1c065abf43acb99d396ad6e2
Corey <cqyates@gmail.com>
2019-04-28 20:10:17 -0400

    more liri.js fixes

--- liri.js
+++ liri.js
@@ -1,3 +1,8 @@
 require("dotenv").config();
+
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+
+//variables for the node command line
+var command = process.argv[2];
+var searchTerm = process.argv.slice(3).join(" ");
