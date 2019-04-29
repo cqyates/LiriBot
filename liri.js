af959ccfaf25e0cf546ed1bf596160a1fda4a960
Corey <cqyates@gmail.com>
2019-04-28 20:13:40 -0400

    added required packages

--- liri.js
+++ liri.js
@@ -1,8 +1,19 @@
 require("dotenv").config();
 
-var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
 
 //variables for the node command line
 var command = process.argv[2];
 var searchTerm = process.argv.slice(3).join(" ");
+
+//required modules
+var Spotify = require('node-spotify-api');
+var keys = require("./keys.js");
+var moment = require('moment');
+var omdb = require('omdb');
+var fs = require('fs');
+var axios = require("axios");
+
+
+
+//api keys in their own file that is not shared on github
+var spotify = new Spotify(keys.spotify);
