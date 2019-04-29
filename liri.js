ebda53b55d4fbbf5312d275a7bbf672231e8e0dd
Corey <cqyates@gmail.com>
2019-04-28 21:02:59 -0400

    added spotify search

--- liri.js
+++ liri.js
@@ -62,3 +62,68 @@ function getBandsInTown(artist){
         }
     });
 }; 
+
+//Spotify Function
+
+function getSpotify(songName){
+    var spotify = new Spotify(keys.spotify);
+
+    if(!songName){
+        songName = "The Sign Ace of Base";
+    };
+
+    spotify.search({type: 'track', query: songName}, function (err, data){
+        if (err) {
+            return console.log("Error occured: " + err);
+        }
+        console.log("================================");
+        console.log("Artist(s) Name: " + data.tracks.items[0].album.artists[0].name + "\r\n");
+        console.log("Song Name: " + data.tracks.items[0].name + "\r\n");
+        console.log("Preview: " + data.tracks.items[0].preview_url+ "\r\n");
+        console.log("Album: "+ data.tracks.items[0].album.name + "\r\n");
+        //add search to log.txt file
+        fs.appendFileSync('log.txt', "\r\n" + "Song Search Log---------------------------------------"+ "\r\n", 'utf8');
+        fs.appendFileSync('log.txt', "\r\n" + "Song Name: " + data.tracks.items[0].name + "\r\n", 'utf8' );
+        fs.appendFileSync('log.txt', "\r\n" + "Artist(s): " + data.tracks.items[0].artists[0].name + "\r\n", 'utf8');
+        fs.appendFileSync('log.txt', "\r\n" + "Album: " + data.tracks.items[0].album.name+ "\r\n", 'utf8');
+        fs.appendFileSync('log.txt', "\r\n" + "Preview Link: " + data.tracks.items[0].preview_url + "\r\n", 'utf8' );
+        fs.appendFileSync('log.txt', "\r\n" + "-------------------------------------------------------"+ "\r\n", 'utf8');
+
+    });
+};
+
+//OMDB Function
+
+function getOMDB(movie){
+    if (!movie) {
+        movie = "Mr. Nobody";
+    }
+    var movieURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+
+    axios.request(movieURL).then(
+        function (response){
+            console.log("================================");
+            console.log("Title: " + response.data.Title + "\r\n");
+            console.log("Year Released: " + response.data.Year + "\r\n");
+            console.log("IMDB Rating: " + response.data.imdbRating + "\r\n");
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\r\n");
+            console.log("Country Where Produced: " + response.data.Country + "\r\n");
+            console.log("Plot: " + response.data.Plot + "\r\n");
+            console.log("Actors: " + response.data.Actors + "\r\n");
+
+    });
+};
+
+//Function for random.txt file
+function getRandom(){
+    fs.readFile("random.txt", "utf8", (error, data) => {
+        if (error) throw error;
+        console.log(data);
+
+        var randomData = data.split(",");
+        liriRun(randomData[0], randomData[1]);
+    });
+};
+
+//call command function
+liriRun(command, userSearch);
